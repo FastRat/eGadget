24c95c7dc5418b4226b5fef4351b63d2de7421ce
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2016-04-21 14:00:55 +0200

    loader
    loader articles

--- js/loader.js
+++ js/loader.js
@@ -9,5 +9,26 @@ app.controller('Warpper', function( $scope ) {
         getView : function( i ) {
             return "section/" + i + ".html";
         }
-    }
+    };
+});
+
+app.controller('ArticleLoader', function( $scope, $http ) {
+    $scope.articles = [];
+
+    var onRepos = function(response) {
+
+      $scope.articles = response.data;
+    };
+
+    var onError = function (reason) {
+        $scope.error = "Nie można pobrać informacji";
+    };
+            
+    var onRequestCompleted = function (response) {
+        $scope.articles = response.data;
+
+        $http.get(location.href + "data/articles.json")
+          .then(onRepos,onError);
+
+    };
 });
